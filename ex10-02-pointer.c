5704436037304aa493634787c432edf4a6b1b724
ITSC <itsc@example.com>
2024-09-08 10:54:24 +0900

    pointer 수업,  배열 숙제

---
+++ ex10-02-pointer.c
@@ -0,0 +1,55 @@
+/* 파일명: ex10-02-pointer.c
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    int num = 10;
+    int *pnum;
+    pnum = &num;
+
+    printf("%d\n", *pnum);
+
+    printf("1) num의 주소값: %p\n", &num);
+    printf("2) pnum의 값: %p\n", pnum);
+
+    num = 14;
+
+    printf("3) num의 주소값: %p\n", &num);
+    printf("4) pnum의 값: %p\n", pnum);
+
+    printf("5) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+
+    *pnum = 30;
+
+    printf("6) num의 주소값: %p\n", &num);
+    printf("7) pnum의 값: %p\n", pnum);
+
+    printf("8) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+    (*pnum)++;
+    (*pnum)++;
+
+    printf("9) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+
+
+    int num2 = 20;
+    pnum = &num2;
+
+
+    printf("10) num의 주소값: %p\n", &num);
+    printf("11) num2의 주소값: %p\n", &num2);
+    printf("12) pnum의 값: %p\n", pnum);
+
+    printf("13) pnum을 이용한 num2의 값 가져오기: %d\n", *pnum);
+
+
+
+
+    return 0;
+}
+
