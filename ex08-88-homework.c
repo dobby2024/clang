9ac776470e88f2075f9b37589634087c048e03a1
ITSC <itsc@example.com>
2024-09-21 09:21:02 +0900

    구구단 숙제정답

--- ex08-88-homework.c
+++ ex08-88-homework.c
@@ -30,3 +30,40 @@
 8 X 9 = 72      9 X 9 = 81
 
 */
+
+
+
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+    for(int k = 0; k < 3; k++) {
+
+        for(int i = 0; i < 9; i++) {
+
+            for(int j = 0; j < 3; j++) {
+                int dan = 2 + j + (k*3);
+                int multiply = i + 1;
+
+                int result = dan * multiply;
+
+                if(dan > 9) break;
+
+                printf("%d X %d = %d\t", dan, multiply, result);
+
+            }
+
+            printf("\n");
+
+        }
+
+        printf("\n");
+    }
+
+    
+    
+
+    return 0;
+}
---
+++ ex13-04-gets-puts.c
@@ -0,0 +1,22 @@
+/* 파일명: ex13-04-gets-puts.c
+
+    gets() - 문자열 입력
+
+    puts() - 문자열 출력
+    
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    char ch[30];
+
+    gets(ch);
+
+    puts(ch);   // 자동으로 줄바꿈
+    puts(ch);
+    puts(ch);
+
+    return 0;
+}
